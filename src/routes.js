81beca2be0a20adadb879cb1c7e20e8edbc2a3d4
Amanda Fernandes <amandaalves@cpejr.com.br>
2022-11-10 20:51:46 -0300

    Controller - Correção de bugs

--- src/routes.js
+++ src/routes.js
@@ -5,16 +5,16 @@ const UserController = require("./controllers/UserController");
 const ExerciseController = require("./controllers/ExerciseController");
 
 // EXERCISES
-routes.get("/exercise/:id_exercicio:", ExerciseController.getByID);
+routes.get("/exercise/:id_exercicio:", ExerciseController.getById);
 routes.post("/exercise", ExerciseController.create);
 routes.put("/exercise/:id_exercicio:", ExerciseController.update);
 routes.delete("/exercise/:id_exercicio:", ExerciseController.delete);
 
-// USERS
-routes.get("/users/:user_id:", UserController.getByID);
-routes.post("/users", UserController.create);
-routes.put("/users/:user_id:", UserController.update);
-routes.delete("/users/:user_id:", UserController.delete);
+// // USERS
+// routes.get("/users/:user_id:", UserController.getByUser);
+// routes.post("/users", UserController.create);
+// routes.put("/users/:user_id:", UserController.update);
+// routes.delete("/users/:user_id:", UserController.delete);
 
  
 module.exports = routes;
