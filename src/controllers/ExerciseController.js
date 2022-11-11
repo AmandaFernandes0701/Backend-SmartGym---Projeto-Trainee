f0b687006cdc123c1bfb338b869f17e160ab259d
Amanda Fernandes <amandaalves@cpejr.com.br>
2022-11-11 14:52:54 -0300

    Controllers 100% prontos

--- src/controllers/ExerciseController.js
+++ src/controllers/ExerciseController.js
@@ -1,72 +1,86 @@
 const ExerciseModel = require("../models/exercise");
 
-const exercise = [ 
-    { 
-        id:0, 
-        name: "Cerrote", 
-    }, 
-    { 
-        id:1, 
-        name: "Extensão Tríceps", 
-    }, 
-    { 
-        id:2, 
-        name: "Flexão", 
-    }, 
-    { 
-        id:3, 
-        name: "Agachamento Livre", 
-    }, 
-    { 
-        id:4, 
-        name: "Leg Press", 
-    }, 
-    { 
-        id:5, 
-        name: "Cadeira Extensora", 
-    }, 
-    { 
-        id:6, 
-        name: "Quadríceps e Coluna", 
-    }, 
-    { 
-        id:7, 
-        name: "Posterior de Ombro", 
-    }, 
-    { 
-        id:8, 
-        name: "Downward Dog", 
-    }, 
+const exercise = [
+    {
+        id: 0,
+        name: "Cerrote",
+    },
+    {
+        id: 1,
+        name: "Extensão Tríceps",
+    },
+    {
+        id: 2,
+        name: "Flexão",
+    },
+    {
+        id: 3,
+        name: "Agachamento Livre",
+    },
+    {
+        id: 4,
+        name: "Leg Press",
+    },
+    {
+        id: 5,
+        name: "Cadeira Extensora",
+    },
+    {
+        id: 6,
+        name: "Quadríceps e Coluna",
+    },
+    {
+        id: 7,
+        name: "Posterior de Ombro",
+    },
+    {
+        id: 8,
+        name: "Downward Dog",
+    },
 ]
 
 module.exports = {
 
 
-    async teste(){
+    async teste() {
         console.log("teste");
     },
 
-    async create(request, response){
+    async getAll(request, response) {
+        try {
+            const result = await ExerciseModel.getAll();
+
+            return response.status(200).json(result);
+        } catch (error) {
+            console.warn("Exercise get failed: ", error);
+
+            return response.status(500).json({
+                notification: "Internal server erros while trying to get Exercise",
+            });
+        }
+    },
+
+    async create(request, response) {
         try {
             console.log("chegou");
             const newExercise = request.body;
             const result = await ExerciseModel.create(newExercise);
-            return response.status(200).json({id_exercicio: result});
+            return response.status(200).json({ id_exercicio: result });
 
         } catch (error) {
             console.log("chegou");
             console.warn("Exercise create failed: ", error);
-            return response.status(400).json({notification: "Internal server erros while trying to create Exercise"});
+            return response.status(400).json({ notification: "Internal server erros while trying to create Exercise" });
         }
     },
 
-    async getById(request, response){
+    async getById(request, response) {
         try {
-            const {id_exercicio} = request.params;
-            const result = await connection("exercise").where({id_exercicio}).select("*");
+            const { id_exercicio } = request.params;
+            const result = await connection("exercise").where({ id_exercicio }).select("*");
             // const result = await ExerciseModel.getByUserWithFilter(exercise_id, {});
 
-            return response.status(200).json({result});
+            return response.status(200).json({ result });
         } catch (error) {
             console.warn("Exercise get failed: ", error);
 
@@ -76,41 +90,41 @@ module.exports = {
         }
     },
 
-    async update(request, response){
+    async update(request, response) {
         try {
-            const {id_exercicio} = request.params;
+            const { id_exercicio } = request.params;
             const newExercise = request.body;
 
             await ExerciseModel.updateById(id_exercicio, newExercise);
 
-            return response.status(200).json({ notification: "Exercise updated sucessfully"});
+            return response.status(200).json({ notification: "Exercise updated sucessfully" });
 
         } catch (error) {
             console.warn("Exercise update failed: ", error);
 
-            return response.status(500).json({notification: "Internal server erros while trying to update Exercise"});
+            return response.status(500).json({ notification: "Internal server erros while trying to update Exercise" });
         }
     },
 
-    async delete(request, response){
+    async delete(request, response) {
         try {
-            const {id_exercicio} = request.params;
+            const { id_exercicio } = request.params;
             const result = await ExerciseModel.deleteById(id_exercicio);
 
-            if (result === 0){
+            if (result === 0) {
                 return response
-                .status(400)
-                .json({ notification: "exercise_id not found" });
+                    .status(400)
+                    .json({ notification: "exercise_id not found" });
             }
-            
+
             return response
-            .status(200)
-            .json({ notification: "Exercise deleted sucessfully"});
+                .status(200)
+                .json({ notification: "Exercise deleted sucessfully" });
 
         } catch (error) {
             console.warn("Exercise delete failed: ", error);
 
-            return response.status(500).json({notification: "Internal server erros while trying to delete Exercise"});
+            return response.status(500).json({ notification: "Internal server erros while trying to delete Exercise" });
         }
     },
 };
--- src/models/relation.js
+++ src/models/relation.js
@@ -1,11 +1,10 @@
 // Relation = Notecategory --> Vídeo do treinamento
 
 const connection = require ("../database/connection");
+const exercise = require("./exercise");
 
 module.exports = {
     async create(relation){
-        exercise.id_exercicio = id_exercicio;
-
         const result = await connection("relation").insert(relation);
         return result;
     },
@@ -14,4 +13,14 @@ module.exports = {
         const result = await connection("relation").where({id_exercicio, user_id}).select("*").first();
         return result;
     },
+
+    async getByUserId({user_id}){
+        const result = await connection("relation").join("exercise","exercise.id_exercicio", "=", "relation.id_exercicio").where({"relation.user_id":user_id}).select("exercise.*");
+        return result;
+    },
+
+    async deleteByUserIdByExercise({user_id,id_exercicio}){
+        const result = await connection("relation").where({user_id,id_exercicio}).delete();
+        return result;
+    }
 }
